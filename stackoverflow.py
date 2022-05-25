e205a5a9f3be9fe8496b07a1b3a5bd2fd482e5d7
goosegusgo <goosegusgo@proton.me>
2022-05-25 23:35:08 +0300

    fix code

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,16 +1,17 @@
 import requests
 import datetime
 import time
+from pprint import pprint
 
 class Stackoverflow:
 
     def get_questions(self, fy, fm, fd, ty, tm, td):
-        url = "https://api.stackexchange.com/docs/questions"
+        url = "https://api.stackexchange.com/2.3/questions"
         offset = datetime.timedelta(hours=3)
         datetime.timezone(offset, name='МСК')
         datetime_from = datetime.date(fy, fm, fd)
         datetime_to = datetime.date(ty, tm, td)
-        params = {"fromdate": str(int(time.mktime(datetime_from.timetuple()))), "todate": str(int(time.mktime(datetime_to.timetuple()))), "order" : "desc", "sort" : "activity", "tagged" : "Python", "site" : "stackoverflow"}
+        params = {"fromdate": str(int(time.mktime(datetime_from.timetuple()))), "todate": str(int(time.mktime(datetime_to.timetuple()))), "order" : "desc", "sort" : "activity", "tagged" : "python", "site" : "stackoverflow"}
         print(params)
         response = requests.get(url, params=params)
         print(response)
@@ -18,7 +19,4 @@ class Stackoverflow:
 
 if __name__ == '__main__':
     stackoverflow = Stackoverflow()
-    print(stackoverflow.get_questions(2022, 5, 23, 2022, 5, 25))
-
-
-# В общем схема рабочая, но косяк в URL как мне кажется, нужен совет как довести это до ума.
+    pprint(stackoverflow.get_questions(2022, 5, 23, 2022, 5, 25))
